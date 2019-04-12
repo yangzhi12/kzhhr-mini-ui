1859da8ab817340e6b07e61dfe9152928e777131
362479845 <362479845@qq.com>
2019-04-12 15:13:00 +0800

    完善页面功能

--- config/api.js
+++ config/api.js
@@ -1,16 +1,16 @@
-const ApiRootUrl = 'http://localhost:8360/api/';
+const ApiRootUrl = 'https://localhost:3393';
 const FileServer = 'https://localhost:3394';
 
 module.exports = {
-  RegisterUrl: ApiRootUrl + 'user/register', //用户注册
-  LoginUrl: ApiRootUrl + 'auth/login', //用户登录
-  IndexUrl: ApiRootUrl + 'index/index', //首页数据接口
-  AuthLoginByWeixin: ApiRootUrl + 'auth/loginByWeixin', //微信登录
-  PersonalUrl: ApiRootUrl + 'user/info', //个人中心资料
-  UserInfoURL: ApiRootUrl + 'user/getUserInfoById', //根据用户ID查询用户信息
-  OrderStatQ: ApiRootUrl + 'contract/statq', //根据用户ID汇总季度签单量
-  OrderStatQList: ApiRootUrl + 'contract/index', //根据用户ID获取签单列表
-  OrderMoney: ApiRootUrl + 'contract/fee', //自动计算合同金额
-  OrderAdd: ApiRootUrl + 'contract/store', //新增保存合同
+  RegisterUrl: ApiRootUrl + '/api/user/register', //用户注册
+  LoginUrl: ApiRootUrl + '/api/auth/login', //用户登录
+  IndexUrl: ApiRootUrl + '/api/index/index', //首页数据接口
+  AuthLoginByWeixin: ApiRootUrl + '/api/auth/loginByWeixin', //微信登录
+  PersonalUrl: ApiRootUrl + '/api/user/info', //个人中心资料
+  UserInfoURL: ApiRootUrl + '/api/user/getUserInfoById', //根据用户ID查询用户信息
+  OrderStatQ: ApiRootUrl + '/api/contract/statq', //根据用户ID汇总季度签单量
+  OrderStatQList: ApiRootUrl + '/api/contract/index', //根据用户ID获取签单列表
+  OrderMoney: ApiRootUrl + '/api/contract/fee', //自动计算合同金额
+  OrderAdd: ApiRootUrl + '/api/contract/store', //新增保存合同
   FileUpload: `${FileServer}/upload`, //文件上传
 };
--- pages/focus/focus.js
+++ pages/focus/focus.js
@@ -1,11 +1,15 @@
 var util = require('../../utils/util.js');
 var api = require('../../config/api.js');
+// var WxParse = require('../../lib/wxParse/wxParse.js');
 var app = getApp();
 Page({
   data: {},
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     // 页面渲染完成
+    // var that = this;
+    // var detail_content = "<div>合同下载</div>";
+    // WxParse.wxParse('detail_content', 'html', detail_content, that, 5);
   },
   onReady: function () {
 
@@ -20,5 +24,22 @@ Page({
   onUnload: function () {
     // 页面关闭
 
+  },
+  downloadfile: function () {
+    // 下载合同
+    wx.downloadFile({
+      url: 'https://hhr.dianjuhui.com:3393/用户服务协议.pdf',
+      success(res) {
+        if (res.statusCode === 200) {
+          const filePath = res.tempFilePath
+          wx.openDocument({
+            filePath,
+            success(res) {
+              console.log('打开文档成功')
+            }
+          })
+        }
+      }
+    })
   }
 })
