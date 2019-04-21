56d311b4eaed7f4644b61d9404592e306ec0472a
362479845 <362479845@qq.com>
2019-04-21 14:03:45 +0800

    增加了我的收益功能展示界面

--- pages/income/income.js
+++ pages/income/income.js
@@ -3,64 +3,102 @@ var api = require('../../config/api.js');
 var app = getApp();
 Page({
   data: {
-    quarter: {
-      QT: 0,
-      QN: 0,
-      YT: 0,
-      YN: 0,
-      RT: 0,
-      RN: 0
-    },
-    currentquarter: 'Q1',
-    years: [2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029],
-    selectedyear: 2019,
-    yearsshow: true,
-    contract: [],
-    stats: ['Q', 'Y', 'R'],
-    relations: {
-      id: 1,
-      name: '张',
-      children: [
+    report: {
+      '2019': 
         {
-          id: 2,
-          name: '1张1',
-          children: [
-            {
-              id: 4,
-              name: '2张1',
-            },
-            {
-              id: 5,
-              name: '2张2',
-              children: [
-                {
-                  id: 8,
-                  name: '8张1',
-                },
-              ]
-            }
-          ]
+          'Q1': {
+            levelname: '业务员',
+            orders: 4,
+            orderprice: 1000,
+            income: 4000000,
+            createtime: '2019-01-01'
+          },
+          'Q2': {
+            levelname: '业务员',
+            orders: 4,
+            orderprice: 1000,
+            income: 4000000,
+            createtime: '2019-04-01'
+          },
+          'Q3': {
+            levelname: '业务员',
+            orders: 4,
+            orderprice: 1000,
+            income: 4000000,
+            createtime: '2019-07-01'
+          },
+          'Q4': {
+            levelname: '业务员',
+            orders: 4,
+            orderprice: 1000,
+            income: 4000000,
+            createtime: '2019-10-01'
+          }
         },
-        {
-          id: 3,
-          name: '1张2',
-          children: [
-            {
-              id: 6,
-              name: '3张1',
-            },
-            {
-              id: 7,
-              name: '3张2',
-            }
-          ]
+      '2018':
+      {
+        'Q1': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-01-01'
         },
-        {
-          id: 4,
-          name: '1张3'
+        'Q2': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-04-01'
+        },
+        'Q3': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-07-01'
+        },
+        'Q4': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-10-01'
         }
-      ]
-    }
+      },
+      '2017':
+      {
+        'Q1': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-01-01'
+        },
+        'Q2': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-04-01'
+        },
+        'Q3': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-07-01'
+        },
+        'Q4': {
+          levelname: '业务员',
+          orders: 4,
+          orderprice: 1000,
+          income: 4000000,
+          createtime: '2019-10-01'
+        }
+      }      
+    },
+    reporttitles: []
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
@@ -70,176 +108,65 @@ Page({
   },
   onShow: function () {
     // 页面显示
-    // this.setData({
-    //   selectedyear: this.data.years[0],
-    //   currentquarter: 'Q1'
-    // })
-    // this.getOrderIndex()
+    this.getReport()
   },
   onHide: function () {
     // 页面隐藏
-
   },
   onUnload: function () {
     // 页面关闭
-
   },
-  // 季度签单量汇总
-  getOrderStatQ() {
+  // 收益报表汇总
+  getReport() {
     let that = this
-    let year = that.data.selectedyear
-    let data = {
-      year: year,
-      quarter: that.data.currentquarter.substr(1, 1)
-    }
-    try {
-      const token = wx.getStorageSync('token')
-      if (token) {
-        wx.request({
-          url: api.OrderStatQ,
-          method: 'post',
-          header: {
-            'x-kzhhr-token': token
-          },
-          data: data,
-          success: function (res) {
-            if (res.statusCode === 200) {
-              let response = res.data
-              if (response.errorno) {
-                wx.showModal({
-                  title: '提示信息',
-                  content: '服务异常',
-                  showCancel: false
-                });
-              } else {
-                let data = response.data
-                Object.keys(data).map(item => {
-                  if (item.substr(1, 1) === 'N') {
-                    data[item] = that.getTwoDecimal(data[item])
-                  }
-                })
-                that.setData({ quarter: data })
-              }
-            } else {
-              wx.showModal({
-                title: '提示信息',
-                content: '网络异常',
-                showCancel: false
-              });
-            }
-          }
-        })
-      }
-    } catch (error) {
+    that.setData({
+      reporttitle: Object.keys(that.data.report)
+    })
+    // let year = that.data.selectedyear
+    // let data = {
+    //   year: year,
+    //   quarter: that.data.currentquarter.substr(1, 1)
+    // }
+    // try {
+    //   const token = wx.getStorageSync('token')
+    //   if (token) {
+    //     wx.request({
+    //       url: api.OrderStatQ,
+    //       method: 'post',
+    //       header: {
+    //         'x-kzhhr-token': token
+    //       },
+    //       data: data,
+    //       success: function (res) {
+    //         if (res.statusCode === 200) {
+    //           let response = res.data
+    //           if (response.errorno) {
+    //             wx.showModal({
+    //               title: '提示信息',
+    //               content: '服务异常',
+    //               showCancel: false
+    //             });
+    //           } else {
+    //             let data = response.data
+    //             Object.keys(data).map(item => {
+    //               if (item.substr(1, 1) === 'N') {
+    //                 data[item] = that.getTwoDecimal(data[item])
+    //               }
+    //             })
+    //             that.setData({ quarter: data })
+    //           }
+    //         } else {
+    //           wx.showModal({
+    //             title: '提示信息',
+    //             content: '网络异常',
+    //             showCancel: false
+    //           });
+    //         }
+    //       }
+    //     })
+    //   }
+    // } catch (error) {
 
-    }
-  },
-  // 季度签单量列表
-  getOrderStatQList() {
-    let that = this
-    let quarter = that.getQuarterIndex(that.data.currentquarter)
-    let year = that.data.selectedyear
-    let data = {
-      year: year,
-      quarter: quarter
-    }
-    try {
-      const token = wx.getStorageSync('token')
-      if (token) {
-        wx.request({
-          url: api.OrderStatQList,
-          method: 'post',
-          header: {
-            'x-kzhhr-token': token
-          },
-          data: data,
-          success: function (res) {
-            if (res.statusCode === 200) {
-              let response = res.data
-              if (response.errorno) {
-                wx.showModal({
-                  title: '提示信息',
-                  content: '服务异常',
-                  showCancel: false
-                });
-              } else {
-                let data = response.data
-                for (let i = 0; i < data.length; i++) {
-                  let statename = util.getApproveFlow(data[i].contractstate)['name']
-                  let money = util.getCommaMoney(data[i].contractvalue, 0)
-                  let recommendmoney = util.getCommaMoney(data[i].recommendvalue, 0)
-                  Object.assign(data[i], { contractstatename: statename, contractvaluecomma: money, recommendvaluecomma: recommendmoney })
-                }
-                that.setData({ contract: data })
-              }
-            } else {
-              wx.showModal({
-                title: '提示信息',
-                content: '网络异常',
-                showCancel: false
-              });
-            }
-          }
-        })
-      }
-    } catch (error) {
-
-    }
-  },
-  //用户点击tab时调用
-  titleClick: function (e) {
-    this.setData({
-      //拿到当前索引并动态改变
-      currentquarter: e.currentTarget.dataset.idx,
-      // 隐藏年份选择（如果显示则隐藏）
-      yearsshow: true
-    })
-    this.getOrderIndex()
-  },
-  titleYearClick: function (e) {
-    this.setData({
-      yearsshow: !this.data.yearsshow
-    })
-  },
-  tapMenuItem: function (e) {
-    this.setData({
-      selectedyear: e.currentTarget.dataset.index,
-      yearsshow: !this.data.yearsshow,
-      currentquarter: 'Q1'
-    })
-    this.getOrderIndex()
-  },
-  getQuarterIndex: function (quarter) {
-    let index = 1
-    switch (quarter) {
-      case 'Q1':
-        index = 1
-        break;
-      case 'Q2':
-        index = 2
-        break;
-      case 'Q3':
-        index = 3
-        break;
-      case 'Q4':
-        index = 4
-        break;
-      default:
-    }
-    return index;
-  },
-  // 我的签单数据
-  getOrderIndex: function () {
-    this.getOrderStatQ() // 获取季度签单统计
-    this.getOrderStatQList() // 获取季度签单列表
-  },
-  // 读取流程名称
-  getApproveFlowNode: function (flowno) {
-    return util.getApproveFlow('010')
-  },
-  // 截取两位小树
-  getTwoDecimal: function (number) {
-    let value = Number(number).toFixed(2)
-    return value
+    // }
   }
 })
